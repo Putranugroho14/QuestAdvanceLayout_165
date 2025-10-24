577f3f2fec7fe031cb2afdaf5bf72181888adfb8
Putranugroho14 <putranugroho61@gmail.com>
2025-10-24 09:55:27 +0700

    membuat card

--- app/src/main/java/com/example/prak4/UIPertama.kt
+++ app/src/main/java/com/example/prak4/UIPertama.kt
@@ -1,9 +1,31 @@
+//noinspection SuspiciousImport
+import android.R
+import android.media.Image
+import android.provider.MediaStore
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
@@ -11,18 +33,25 @@ fun ActivitasPertama(modifier: Modifier){
         modifier = Modifier
             .padding(top = 100.dp)
             .fillMaxSize(),
-        horizontalAligment = Aligment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ){
         Text(
-            text = stringResource(id = R.string.prodi),
-            fontsize = 35.sp,
-            fontWeight = fontWeight.Bold
+            text = R.stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
         )
         Text(
             text = stringResource(id = R.string.univ),
-            fontsize = 22.sp,
+            fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
 
-    }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+
+            )
+
 }
