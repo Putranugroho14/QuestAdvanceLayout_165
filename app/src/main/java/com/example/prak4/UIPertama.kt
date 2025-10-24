8969f34bfcd657309aa7322282ace5d6ba581bb0
Putranugroho14 <putranugroho61@gmail.com>
2025-10-24 09:21:32 +0700

    membuat composable

---
+++ app/src/main/java/com/example/prak4/UIPertama.kt
@@ -0,0 +1,21 @@
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAligment = Aligment.CenterHorizontally
+    ){
+        T
+
+
+
+    }
+}
